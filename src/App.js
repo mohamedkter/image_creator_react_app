4939ba79c592182695688fded591c553d18ee32c
mohamedkter <mom358995@gmail.com>
2024-07-15 03:54:14 +0300

    ui done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CreatedImage from "./components/created_image";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Create Image With AI</h1>
+      <CreatedImage />
+      <div className="input_section">
+        <input type="text" placeholder="prompt"/>
+        <button>Create</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/created_image.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreatedImage = () => {
+  return (
+    <div className='created_image_section'>
+      <div className='image_div'></div>
+      <div className='image_div'></div>
+      <div className='image_div'></div>
+    </div>
+  )
+}
+
+export default CreatedImage
